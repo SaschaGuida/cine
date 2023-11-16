732197cb576d3515634fc96615513007fd583e40
sak89 <sascha.guida@gmail.com>
2023-11-16 15:06:12 +0100

    fix home + card style

--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,4 @@
-// api.js
-export const BASE_URL = 'https://mttlioitimpeuzlwsgql.supabase.co/rest/v1/movies';
+/* export const BASE_URL = 'https://mttlioitimpeuzlwsgql.supabase.co/rest/v1/movies';
 export const HEADERS = {
     headers: {
         'Content-Type': 'application/json',
@@ -24,6 +23,28 @@ export const fetchMovies = async () => {
         return extractedMovies;
     } catch (error) {
         console.error('Error fetching movies:', error.message);
+        throw error;
+    }
+}; */
+
+
+// api.js
+export const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/';
+
+export const fetchPokemon = async (pokemonId) => {
+    try {
+        const response = await fetch(`${BASE_URL}${pokemonId}`);
+        if (!response.ok) {
+            throw new Error('Failed to fetch Pokemon data');
+        }
+        const data = await response.json();
+
+        // Log the entire API response
+        console.log('API Response:', data);
+
+        return data;
+    } catch (error) {
+        console.error('Error fetching Pokemon data:', error.message);
         throw error;
     }
 };
